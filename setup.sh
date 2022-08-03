4801dfe4ea27a09b50060ab0d1b3db4ef41caab6
Khaled Hajjaj <steve.hajjaj@gmail.com>
2022-08-03 17:33:46 +1000

    Automated commit

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ echo "\
 email = \"steve.hajjaj@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " >> ~/.streamlit/config.toml
